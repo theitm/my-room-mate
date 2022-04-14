756794ff4b5596a495c27f4c669a0f950a58bf64
Tram Le Thi Nhu <intern.ltntram@tma.com.vn>
2022-04-14 09:34:59 +0700

    update roomSharing

--- src/main/java/motelRoom/mapper/SharingDetailMapper.java
+++ src/main/java/motelRoom/mapper/SharingDetailMapper.java
@@ -4,7 +4,6 @@ import motelRoom.dto.sharingDetail.SharingDetailCreateDto;
 import motelRoom.dto.sharingDetail.SharingDetailDetailDto;
 import motelRoom.entity.SharingDetailEntity;
 import org.mapstruct.Mapper;
-
 import java.util.List;
 
 @Mapper
