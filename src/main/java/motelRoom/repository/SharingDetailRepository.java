b0bef0a0ac41e07f3b101fca9b4115d83188dd8e
Tram Le Thi Nhu <intern.ltntram@tma.com.vn>
2022-04-18 14:29:09 +0700

    Check userId in RoomSharing

--- src/main/java/motelRoom/repository/SharingDetailRepository.java
+++ src/main/java/motelRoom/repository/SharingDetailRepository.java
@@ -2,9 +2,15 @@ package motelRoom.repository;
 
 import motelRoom.entity.SharingDetailEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
 public interface SharingDetailRepository extends JpaRepository<SharingDetailEntity, UUID> {
+//    @Query("SELECT u FROM User u WHERE u.status = 1")
+//    Collection<User> findAllActiveUsers();
+
+    @Query("SELECT u from SharingDetailEntity u WHERE u.user_id = :id")
+    SharingDetailEntity findByUserId(UUID id);
 }
