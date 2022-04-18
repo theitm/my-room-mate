8ae3c477f85cc99cc7f4965ee090adea450d84e4
Tranlam1007 <Tranlamzero2k@gmail.com>
2022-04-18 15:34:18 +0700

    Delete commetn

--- src/main/java/motelRoom/repository/SharingDetailRepository.java
+++ src/main/java/motelRoom/repository/SharingDetailRepository.java
@@ -8,9 +8,6 @@ import java.util.UUID;
 
 @Repository
 public interface SharingDetailRepository extends JpaRepository<SharingDetailEntity, UUID> {
-//    @Query("SELECT u FROM User u WHERE u.status = 1")
-//    Collection<User> findAllActiveUsers();
-
     @Query("SELECT u from SharingDetailEntity u WHERE u.user_id = :id")
     SharingDetailEntity findByUserId(UUID id);
 }
