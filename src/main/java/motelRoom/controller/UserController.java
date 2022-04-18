904cc44772578630171760fe741fa9f3c10785ea
Tranlam1007 <Tranlamzero2k@gmail.com>
2022-04-18 15:31:40 +0700

    API USER

--- src/main/java/motelRoom/controller/UserController.java
+++ src/main/java/motelRoom/controller/UserController.java
@@ -1,21 +1,21 @@
 package motelRoom.controller;
 
-import motelRoom.JWT.JwtTokenProvider;
-import motelRoom.JWT.LoginResponse;
-import motelRoom.JWT.RandomStuff;
 import motelRoom.dto.user.UserCreateDto;
 import motelRoom.dto.user.UserDetailDto;
-import motelRoom.dto.user.UserLogin;
-import motelRoom.service.userService.CustomUserDetails;
 import motelRoom.service.userService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import motelRoom.JWT.JwtTokenProvider;
+import motelRoom.JWT.LoginResponse;
+import motelRoom.JWT.RandomStuff;
+import motelRoom.dto.user.UserLogin;
+import motelRoom.service.userService.CustomUserDetails;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
@@ -54,6 +54,14 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(userService.createUser(userCreateDto));
     }
 
+    /** ---------------- UPDATE NEW USER ------------------------ */
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDetailDto> updateUser(@PathVariable UUID id,
+                                                    @RequestBody UserCreateDto userCreateDto)
+    {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(userService.createUser(userCreateDto));
+    }
+
     /** ---------------- LOGIN USER ------------------------ */
     @PostMapping("/login")
     public LoginResponse authenticateUser (@Valid @RequestBody UserLogin userLogin) {
@@ -77,5 +85,11 @@ public class UserController {
     @GetMapping("/random")
     public RandomStuff randomStuff(){
         return new RandomStuff("JWT Hợp lệ mới có thể thấy được message này");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteById(@PathVariable UUID id) {
+        userService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
 }
--- src/main/java/motelRoom/service/userService/UserServiceImpl.java
+++ src/main/java/motelRoom/service/userService/UserServiceImpl.java
@@ -9,6 +9,7 @@ import motelRoom.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.BeanUtils;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,28 +18,12 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     PasswordEncoder passwordEncoder;
-
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
     public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
-    }
-
-    /**
-     * Tạo mới 1 User
-     */
-    @Override
-    public UserDetailDto createUser(UserCreateDto userCreateDto) {
-        UserEntity userEntity = userMapper.fromUserEntityCreateDtoToEntity(userCreateDto);
-        userEntity.setPassword(passwordEncoder.encode(userCreateDto.getPassword()));
-        UserEntity CreateUser = userRepository.save(userEntity);
-        UserDetailDto userDetailDto =null;
-        if (CreateUser != null){
-            userDetailDto = userMapper.fromUserEntityToUserCrateDto(CreateUser);
-        }
-        return userDetailDto;
     }
 
     /**
@@ -55,5 +40,55 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<UserLogin> findAllAcc() {
         return userMapper.fromListDtosToEntities(userRepository.findAll());
+    }
+    /**
+     * create new user
+     * Account name does not match
+     * @param userCreateDto
+     * @return
+     */
+
+    @Override
+    public UserDetailDto createUser(UserCreateDto userCreateDto) {
+        String username = userCreateDto.getUsername();
+        UserEntity entity = userRepository.findByUsername(username);
+        if (entity == null ) {
+            UserEntity userEntity = userMapper.fromUserEntityCreateDtoToEntity(userCreateDto);
+            userEntity.setPassword(passwordEncoder.encode(userCreateDto.getPassword()));
+            UserEntity userEntityCreate = userRepository.save(userEntity);
+            UserDetailDto userDetailDto =null;
+            if(userEntityCreate != null) {
+                userDetailDto = userMapper.fromUserEntityToUserCrateDto(userEntityCreate);
+            }
+            return userDetailDto;
+        }
+        return null;
+    }
+
+    /**
+     * update user by id
+     * @param
+     * @return
+     */
+    @Override
+    public  UserDetailDto updateUser(UUID id,  UserDetailDto userDetailDto) {
+        UserEntity userEntity = userRepository.findById(id).orElse(null);
+        if(userEntity == null){
+            return null;
+        }
+        BeanUtils.copyProperties(userDetailDto, userEntity);
+        userRepository.saveAndFlush(userEntity);
+        userDetailDto = userMapper.fromUserEntityToUserCrateDto(userEntity);
+        return userDetailDto;
+    }
+
+    /**
+     * delete user by id
+     * @param
+     * @return
+     */
+    @Override
+    public void deleteById(UUID id) {
+        userRepository.deleteById(id);
     }
 }
