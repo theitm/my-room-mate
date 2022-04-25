54cb43bd3e35ed625c5f442760a1f7ff1d561f9f
thcong98 <thanhcongvq347@gmail.com>
2022-04-25 19:44:56 +0700

    Response Login = token + UserDetailDto

--- src/main/java/motelRoom/JWT/LoginResponse.java
+++ src/main/java/motelRoom/JWT/LoginResponse.java
@@ -1,13 +1,16 @@
 package motelRoom.JWT;
 
 import lombok.Data;
+import motelRoom.dto.user.UserDetailDto;
 
 @Data
 public class LoginResponse {
     private String accessToken;
     private String tokenType = "Bearer";
+    private UserDetailDto userDetailDto;
 
-    public LoginResponse(String accessToken) {
+    public LoginResponse(String accessToken, UserDetailDto userDetailDto) {
         this.accessToken = accessToken;
+        this.userDetailDto = userDetailDto;
     }
 }
